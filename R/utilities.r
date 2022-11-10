3ccc75969b4ec936ce7f9a4969942d3e34278200
Chengxugorilla <94209830+Chengxugorilla@users.noreply.github.com>
2022-11-10 18:07:32 +0800

    Update utilities.r

--- R/utilities.r
+++ R/utilities.r
@@ -7,7 +7,10 @@ svg2tempfile <- function(svg) {
 #' @import grDevices
 
 colorb <- function(Expression, low = "blue", high = "red") {
-  zero_scale_line <- max(Expression) - min(Expression)
+  zero_scale_line <- 0
+  if(all(pretty(Expression, 4) >= 0) || all(pretty(Expression, 4) <= 0)){
+    zero_scale_line <- max(Expression) - min(Expression)
+  }
   Expression_low <- Expression[which(Expression <= zero_scale_line)]
   Expression_high <- Expression[which(Expression > zero_scale_line)]
   scaleExpr_low <- (Expression_low - min(Expression_low)) / diff(range(Expression_low))
